c5e4ef8bcb2da499ac5c9df9d55b169774104ad7
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-05 20:24:11 +0900

    코틀린 클래스를 JPA 엔티티 클래스로 사용할 수 있도록 allopen 플러그인 설정 추가, H2 콘솔 활성화

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
+	kotlin("plugin.allopen") version "1.3.72"
 }
 
 group = "com.example"
@@ -40,4 +41,10 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "11"
 	}
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.Embeddable")
+	annotation("javax.persistence.MappedSuperclass")
 }
